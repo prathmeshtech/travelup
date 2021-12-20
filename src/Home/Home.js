08a4dea9c65a409b8caaddde00e2ab400d83219e
Prathmesh Singh <64306983+prathmeshtech@users.noreply.github.com>
2021-12-20 19:41:23 +0530

    commit1

---
+++ src/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <h1>Just a home component</h1>
+        </div>
+    )
+}
+
+export default Home
